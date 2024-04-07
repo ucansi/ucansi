0aa03fcb0ca73b2fcf1ed5e59d25e910c73935cf
ucansi <165784812+ucansi@users.noreply.github.com>
2024-04-07 09:20:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var keyParam = getParameterByName('key');
+
+if (keyParam) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        document.body.innerHTML = xhr.responseText;
+      } else {
+        console.error('Request failed with status:', xhr.status);
+      }
+    }
+  };
+  xhr.open('GET', '/api.php?key=' + keyParam, true);
+  xhr.send();
+}
